73b1439d0732aa2e6515720e85f2d18665850114
Amy <amy.gh.ly@gmail.com>
2025-10-23 12:50:32 +1100

    Add texts

--- src/components/photoCard.jsx
+++ src/components/photoCard.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect, useRef } from 'react';
 import vibe from "../assets/vibe.png"
+import.meta.env.GEMINI_API_KEY
+
 
 const PhotoCard = () => {
     return (
@@ -7,7 +8,15 @@ const PhotoCard = () => {
             <div className="w-70 h-90 overflow-clip">
                 <img src={vibe} style={{objectFit:"cover", width:"100%", height:"100%", borderRadius:"5px"}}/>
             </div>
-            <h2 className="text-2xl tracking-widest mb-8 text-gray-800 font-light text-left mt-8">NAME</h2>
+            <div className="justify-between flex">
+                <h2 className="text-2xl tracking-widest mb-3 text-gray-800 font-light mt-8">NAME</h2>
+                <h2 className="text-2xl tracking-widest mb-3 text-gray-800 font-light mt-8 text-right max-w-45">{"Chigi no shiro"}</h2>
+            </div>
+            <div className="justify-between flex">
+                <h3 className="text-xl tracking-widest mb-8 text-gray-800 font-light text-left">REGION</h3>
+                <h3 className="text-xl tracking-widest mb-8 text-gray-800 font-light text-right max-w-45">{"Uji"}</h3>
+            </div>
+            
         </div>
     )
 
