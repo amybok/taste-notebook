13cb040f58273ca6e5da3ff949a7110aa13185ce
Amy Ly <119104583+amybok@users.noreply.github.com>
2026-07-28 06:07:25 +0000

    Change site icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/Untitled design (11).png'
 import './App.css'
 import TastingChart from './components/tastingChart'
 import PhotoCard from './components/photoCard'
