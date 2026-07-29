5eacbc75cb6b09ca28d2860210607dce112e54e3
Amy <amy.gh.ly@gmail.com>
2026-07-29 23:03:50 +1000

    Add Vercel CLI & test db connection

--- api/hello.js
+++ api/hello.js
@@ -1,16 +1,27 @@
 import { supabase } from "../lib/supabase.js";
 
-const { data, error } = await supabase
-  .from("information_schema.tables")
-  .select("table_name")
+// const { data, error } = await supabase
+//   .from("information_schema.tables")
+//   .select("table_name")
 
-if (error) {
+// if (error) {
+//   // Logs the full error: message, code, details, and hint.
+//   console.error(error)
+// } else {
+//   console.log(data)
+// }
+
+async function test(){
+  const { data, error } = await supabase.from("information_schema.tables").select("table_name");
+
+  if (error) {
   // Logs the full error: message, code, details, and hint.
-  console.error(error)
-} else {
-  console.log(data)
+  console.error(error)} 
+  else {
+  console.log(data)};
 }
 
 export function GET(request) {
+  test();
   return new Response('Hello from Vercel!');
 }
