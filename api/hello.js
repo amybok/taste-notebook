e7c245f6b52b3a55c5cc21e9b6173f0b0461bd75
Amy <amy.gh.ly@gmail.com>
2026-08-05 00:02:01 +1000

    Add HTTP Methods

--- api/hello.js
+++ api/hello.js
@@ -1,17 +1,6 @@
 import { supabase } from "../lib/supabase.js";
 
-// const { data, error } = await supabase
-//   .from("information_schema.tables")
-//   .select("table_name")
-
-// if (error) {
-//   // Logs the full error: message, code, details, and hint.
-//   console.error(error)
-// } else {
-//   console.log(data)
-// }
-
-async function test(){
+async function query(){
   const { data, error } = await supabase.from("records").select("*");
 
   if (error) {
@@ -23,8 +12,20 @@ async function test(){
     console.log(data)};
 }
 
+async function submit(){}
+
 export async function GET(request) {
-  await test();
+  await query();
   console.log("yes")
   return new Response('Hello from Vercel!');
+}
+
+export async function POST(request) {
+  // const { error } = await supabase
+  // .from('countries')
+  // .insert({ id: 1, name: 'Mordor' })
+}
+
+export async function PATCH(request) {
+
 }
